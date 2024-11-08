ef9ee4acfc4d04afb75b2928760840bd2bea266d
Phuong Anh <phuonganh24012004@gmail.com>
2024-11-08 16:26:36 +0700

    Lab02

---
+++ Controllers/NguoiDungController.cs
@@ -0,0 +1,108 @@
+﻿using MvcBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBookStore.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        private QLBANSACHEntities db = new QLBANSACHEntities();
+        // GET: NguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Dangky(FormCollection collection, KHACHHANG kh)
+        {
+            var hoten = collection["HotenKH"];
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            var matkhaunhaplai = collection["Nhaplaimatkhau"];
+            var email = collection["email"];
+            var diachi = collection["Diachi"];
+            var dienthoai = collection["Dienthoai"];
+            var ngaysinh = String.Format("{0:MM/dd/yyyy}", collection["Ngaysinh"]);
+            if (String.IsNullOrEmpty(hoten))
+            {
+                ViewData["Loi1"] = "Họ tên khách hàng không được để trống";
+            }
+            else if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi2"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi3"] = "Phải nhập mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(matkhaunhaplai))
+            {
+                ViewData["Loi4"] = "Phải nhập lại mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(email))
+            {
+                ViewData["Loi5"] = "Email không được bỏ trống";
+            }
+            else if (String.IsNullOrEmpty(dienthoai))
+            {
+                ViewData["Loi6"] = "Phải nhập số điện thoại";
+            }
+            else
+            {
+                kh.HoTenKH = hoten;
+                kh.TenDN = tendn;
+                kh.Matkhau = matkhau;
+                kh.Email = email;
+                kh.DiachiKH = diachi;
+                kh.DienthoaiKH = dienthoai;
+                kh.Ngaysinh = DateTime.Parse(ngaysinh);
+                db.KHACHHANG.Add(kh);
+                db.SaveChanges();
+                return RedirectToAction("Dangnhap");
+            }
+            return this.Dangky();
+        }
+        [HttpPost]
+        public ActionResult Dangnhap(FormCollection collection)
+        {
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                KHACHHANG kh = db.KHACHHANG.SingleOrDefault(n => n.TenDN == tendn && n.Matkhau == matkhau);
+                if (kh != null)
+                {
+                    //ViewBag.Thongbao = "Chúc mừng đăng nhập thành công";
+                    Session["Taikhoan"] = kh;
+                    return RedirectToAction("Index", "BookStore");
+                }
+                else
+                {
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/GioHang.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcBookStore.Models
+{
+    public class GioHang
+    {
+        private QLBANSACHEntities db = new QLBANSACHEntities();
+        public int iMasach { get; set; } 
+        public string sTensach { get; set; } 
+        public string sAnhbia { get; set; } 
+        public Double dDongia { get; set; } 
+        public int iSoluong { get; set; } = 1; 
+        public double dThanhtien
+        {
+            get { return iSoluong * dDongia; } 
+        }
+
+        public GioHang(int Masach)
+        {
+            iMasach = Masach;
+
+            SACH sach = db.SACH.Single(n => n.Masach == Masach);
+
+            sTensach = sach.Tensach;
+            sAnhbia = sach.Hinhminhhoa;
+            dDongia = double.Parse(sach.Dongia.ToString());
+            iSoluong = 1;
+        }
+    }
+}
